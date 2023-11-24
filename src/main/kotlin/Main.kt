4762aeed259ab2b8a3fda0d3836345d408ea91a2
Gordon Chou <chouguting@gmail.com>
2023-11-25 03:05:17 +0800

    update ui

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,14 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -57,136 +59,152 @@ fun App() {
                 )
             }
 
-            Row (verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(20.dp)){
-                Button(onClick = {
-                    val fileChooser = JFileChooser()
-                    fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY)
-                    fileChooser.setCurrentDirectory(File("."))
-                    fileChooser.setDialogTitle("Choose a directory")
-                    fileChooser.setAcceptAllFileFilterUsed(false) //disable all file type option
+            Row(modifier = Modifier.fillMaxSize()){
+                Column(modifier = Modifier.weight(2f).fillMaxSize()) {
+                    //choose input folder
+                    Row (verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(20.dp)){
+                        Button(onClick = {
+                            val fileChooser = JFileChooser()
+                            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY)
+                            fileChooser.setCurrentDirectory(File("."))
+                            fileChooser.setDialogTitle("Choose a directory")
+                            fileChooser.setAcceptAllFileFilterUsed(false) //disable all file type option
 
-                    val userSelection = fileChooser.showOpenDialog(JFrame())
-                    if (userSelection == JFileChooser.APPROVE_OPTION) {
-                        val selectedDir = fileChooser.selectedFile.toString()
-                        val fileLists = Utils.findSupportedFilesInDir(selectedDir);
+                            val userSelection = fileChooser.showOpenDialog(JFrame())
+                            if (userSelection == JFileChooser.APPROVE_OPTION) {
+                                val selectedDir = fileChooser.selectedFile.toString()
+                                val fileLists = Utils.findSupportedFilesInDir(selectedDir);
 
-                        for (filename in fileLists) {
-                            fileList.add(filename)
-                            fileAndItsPath.put(filename, selectedDir + "\\" + filename)
+                                for (filename in fileLists) {
+                                    fileList.add(filename)
+                                    fileAndItsPath.put(filename, selectedDir + "\\" + filename)
+                                }
+
+                                loadedFolderInfo.value = "loaded folder: $selectedDir , ${fileList.size} files loaded"
+                                folderLoaded.value = true
+                                outputResult.value = ""
+                            }
+
+                        }) {
+                            Text("input file folder")
                         }
 
-                        loadedFolderInfo.value = "loaded folder: $selectedDir , ${fileList.size} files loaded"
-                        folderLoaded.value = true
-                        outputResult.value = ""
+                        Text(loadedFolderInfo.value)
+
+                    }
+                    //spacer
+                    Spacer(modifier = Modifier.fillMaxWidth().height(20.dp))
+
+                    //choose output folder
+                    AnimatedVisibility(visible = folderLoaded.value) {
+                        Row (verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(10.dp)){
+                            Button(onClick = {
+                                val fileChooser = JFileChooser()
+                                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY)
+                                fileChooser.setCurrentDirectory(File("."))
+                                fileChooser.setDialogTitle("Choose a directory")
+                                fileChooser.setAcceptAllFileFilterUsed(false) //disable all file type option
+                                val userSelection = fileChooser.showOpenDialog(JFrame())
+                                if (userSelection == JFileChooser.APPROVE_OPTION) {
+                                    val selectedDir = fileChooser.selectedFile.toString()
+                                    saveToThisFolder.value = selectedDir
+                                    saveToFolderInfo.value = "save to dir: $selectedDir"
+                                    saveToFolderSelected.value = true
+                                    outputResult.value = ""
+                                }
+
+
+
+                            }) {
+                                Text("output folder")
+                            }
+                            Text(saveToFolderInfo.value)
+
+                        }
+
+
                     }
 
-                }) {
-                    Text("input file folder")
-                }
+                    //spacer
+                    Spacer(modifier = Modifier.fillMaxWidth().height(20.dp))
 
-                Text(loadedFolderInfo.value)
-
-            }
-            //spacer
-            Spacer(modifier = Modifier.fillMaxWidth().height(20.dp))
-
-            AnimatedVisibility(visible = folderLoaded.value) {
-                Row (verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(10.dp)){
-                    Button(onClick = {
-                        val fileChooser = JFileChooser()
-                        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY)
-                        fileChooser.setCurrentDirectory(File("."))
-                        fileChooser.setDialogTitle("Choose a directory")
-                        fileChooser.setAcceptAllFileFilterUsed(false) //disable all file type option
-                        val userSelection = fileChooser.showOpenDialog(JFrame())
-                        if (userSelection == JFileChooser.APPROVE_OPTION) {
-                            val selectedDir = fileChooser.selectedFile.toString()
-                            saveToThisFolder.value = selectedDir
-                            saveToFolderInfo.value = "save to dir: $selectedDir"
-                            saveToFolderSelected.value = true
-                            outputResult.value = ""
+                    //choose task
+                    AnimatedVisibility(visible = saveToFolderSelected.value) {
+                        Column(modifier = Modifier.fillMaxWidth()){
+                            Text("Choose the task you want to run: ")
+                            Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                                Button(onClick={
+                                    convertToJsonAndSave(fileList, fileAndItsPath, saveToThisFolder.value)
+                                    outputResult.value = "convertToJsonAndSave done"
+                                }){
+                                    Text("Convert to Json")
+                                }
+                                Button(onClick={
+                                    aesAndSave(mode = 0,fileList, fileAndItsPath, saveToThisFolder.value)
+                                    outputResult.value = "AES done, result(json) saved to folder"
+                                }){
+                                    Text("AES result to Json")
+                                }
+                                Button(onClick={
+                                    aesAndSave(mode = 1,fileList, fileAndItsPath, saveToThisFolder.value)
+                                    outputResult.value = "AES done, result(rsp) saved to folder"
+                                }){
+                                    Text("AES result to RSP")
+                                }
+                            }
+                            Text(outputResult.value)
                         }
 
 
 
-                    }) {
-                        Text("output folder")
                     }
-                    Text(saveToFolderInfo.value)
+                }
+
+                //display file list
+                Column(modifier = Modifier.weight(1f).clip(RoundedCornerShape(20.dp)).background(Color.Cyan).fillMaxSize()) {
 
                 }
 
 
             }
 
-            //spacer
-            Spacer(modifier = Modifier.fillMaxWidth().height(20.dp))
-
-            AnimatedVisibility(visible = saveToFolderSelected.value) {
-                Column(modifier = Modifier.fillMaxWidth()){
-                    Text("Choose the task you want to run: ")
-                    Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
-                        Button(onClick={
-                            convertToJsonAndSave(fileList, fileAndItsPath, saveToThisFolder.value)
-                            outputResult.value = "convertToJsonAndSave done"
-                        }){
-                            Text("Convert to Json")
-                        }
-                        Button(onClick={
-                            aesAndSave(mode = 0,fileList, fileAndItsPath, saveToThisFolder.value)
-                            outputResult.value = "AES done, result(json) saved to folder"
-                        }){
-                            Text("AES result to Json")
-                        }
-                        Button(onClick={
-                            aesAndSave(mode = 1,fileList, fileAndItsPath, saveToThisFolder.value)
-                            outputResult.value = "AES done, result(rsp) saved to folder"
-                        }){
-                            Text("AES result to RSP")
-                        }
-                    }
-                    Text(outputResult.value)
-                }
-
-
-
-            }
 
 
 
 
 
-            Row(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .weight(1f)
-                    .background(Color.White)
 
-            ) {
-                Column(modifier = Modifier
-                    .fillMaxSize()
-                    .weight(1f)
-                    ) {
-
-                }
-                Column(modifier = Modifier
-                    .fillMaxSize()
-                    .weight(1f)
-                    ) {
-
-                }
-                Column(modifier = Modifier
-                    .fillMaxSize()
-                    .weight(1f)
-                    ) {
-//                    Icon(
-//                        Icons.Rounded.ShoppingCart,
-//                        contentDescription = ""
-//                    )
-                }
-
-
-            }
+//            Row(
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .weight(1f)
+//                    .background(Color.White)
+//
+//            ) {
+//                Column(modifier = Modifier
+//                    .fillMaxSize()
+//                    .weight(1f)
+//                    ) {
+//
+//                }
+//                Column(modifier = Modifier
+//                    .fillMaxSize()
+//                    .weight(1f)
+//                    ) {
+//
+//                }
+//                Column(modifier = Modifier
+//                    .fillMaxSize()
+//                    .weight(1f)
+//                    ) {
+////                    Icon(
+////                        Icons.Rounded.ShoppingCart,
+////                        contentDescription = ""
+////                    )
+//                }
+//
+//
+//            }
 
 
 
